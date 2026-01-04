4ce890b159ce9928dd75729552f60a61e192011f
Moses <mosochie@gmail.com>
2026-01-04 18:54:27 +0300

    rounding nav bar corners

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
         <header class="shadow-sm bg-white "> 
-        <nav class="container mx-auto p-4 flex justify-between ">
-            <NuxtLink to="/" class="text-2xl font-bold font-serif antialiased md:subpixel-antialiased">CIL FABRICATORS</NuxtLink>
-            <ul class="flex gap-2 font-bold bg-amber-50 p-1.5 rounded-2xl font-serif">
-                <li><NuxtLink to="/"> Home</NuxtLink></li>
-                <li><NuxtLink to="/about"> About</NuxtLink></li>
-                <li><NuxtLink to="/products"> Products</NuxtLink></li>
+        <nav class="container mx-auto p-2 flex justify-between ">
+            <NuxtLink to="/" class="text-2xl font-bold font-serif antialiased md:subpixel-antialiased text-shadow-2xs">CIL FABRICATORS</NuxtLink>
+            <ul class="flex gap-2 font-bold bg-amber-50 p-1.5 font-serif rounded-tl-lg rounded-br-lg">
+                <li><NuxtLink to="/"> Home |</NuxtLink></li>
+                <li><NuxtLink to="/about"> About |</NuxtLink></li>
+                <li><NuxtLink to="/products"> Products |</NuxtLink></li>
                 <li><NuxtLink to="/contacts">Contact</NuxtLink></li>
             </ul>
         </nav>
--- layouts/product.vue
+++ layouts/product.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <header class="shadow-sm bg-white p-4"> 
-        <nav class="container mx-auto p-4 ">
+        <header class="shadow-sm bg-white"> 
+        <nav class="container mx-auto p-2 ">
 
             <NuxtLink to="/" class="text-3xl font-bold font-serif antialiased md:subpixel-antialiased">CIL FABRICATORS</NuxtLink>
             
@@ -16,13 +16,13 @@
 
         </div>
 
-        <footer>
+        <footer class="shadow-sm bg-white ">
 
-            <ul class="flex gap-4">
-                <li><NuxtLink to="./"> Home</NuxtLink></li>
-                <li><NuxtLink to="./about"> About</NuxtLink></li>
-                <li><NuxtLink to="./products"> Products</NuxtLink></li>
-                <li><NuxtLink to="./contacts">Contact</NuxtLink></li>
+            <ul class="flex gap-2 font-bold p-1.5 rounded-2xl font-serif mx-7">
+                <li><NuxtLink to="/"> Home |</NuxtLink></li>
+                <li><NuxtLink to="/about"> About |</NuxtLink></li>
+                <li><NuxtLink to="/products"> Products |</NuxtLink></li>
+                <li><NuxtLink to="/contacts">Contact</NuxtLink></li>
             </ul>
 
         </footer>

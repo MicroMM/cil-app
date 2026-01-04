36647f9808c6f3249d369cda547e669495f6ea1e
Moses <mosochie@gmail.com>
2026-01-04 20:16:35 +0300

    images added

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,19 +1,19 @@
 <template>
     <div>
         <h2> Products Page</h2>
-        <div class="grid grid-cols-4 gap-x-4 gap-y-2 p-7 ">
-            <div class="...">product-1</div>
-            <div class="...">product-2</div>
-            <div class="...">product-3</div>
-            <div class="...">product-4</div>
-            <div class="...">product-5</div>
-            <div class="...">product-6</div>
-            <div class="...">product-7</div>
-            <div class="...">product-8</div>
-            <div class="...">product-9</div>
-            <div class="...">product-10</div>
-            <div class="...">product-11</div>
-            <div class="...">product-12</div>
+        <div class="grid grid-cols-4 gap-x-4 gap-y-2 p-2 ">
+            <div class="..."><img class="h-48 w-96 object-cover..." src="/images/blurred-spring.jpg" alt="blurred spring"/></div>
+            <div class="..."><img class="h-48 w-96 object-cover..." src="/images/majestic-mountain.jpg" alt="blurred spring"/></div>
+            <div class="..."><img class="h-48 w-96 object-cover..." src="/images/spa-natural.jpg" alt="blurred spring"/></div>
+            <div class="..."><img class="h-48 w-96 object-cover..." src="/images/blurred-spring.jpg" alt="blurred spring"/></div>
+            <div class="..."><img class="h-48 w-96 object-cover..." src="/images/majestic-mountain.jpg" alt="blurred spring"/></div>
+            <div class="..."><img class="h-48 w-96 object-cover..." src="/images/spa-natural.jpg" alt="blurred spring"/></div>
+            <div class="..."><img class="h-48 w-96 object-cover..." src="/images/blurred-spring.jpg" alt="blurred spring"/></div>
+            <div class="..."><img class="h-48 w-96 object-cover..." src="/images/majestic-mountain.jpg" alt="blurred spring"/></div>
+            <div class="..."><img class="h-48 w-96 object-cover..." src="/images/spa-natural.jpg" alt="blurred spring"/></div>
+            <div class="..."><img class="h-48 w-96 object-cover..." src="/images/blurred-spring.jpg" alt="blurred spring"/></div>
+            <div class="..."><img class="h-48 w-96 object-cover..." src="/images/majestic-mountain.jpg" alt="blurred spring"/></div>
+            <div class="..."><img class="h-48 w-96 object-cover..." src="/images/spa-natural.jpg" alt="blurred spring"/></div>
         </div>
 
     </div>
